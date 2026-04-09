d25bed36e184a3eaa368999ed2a31c8085468870
rociozarate-mi <zaraterocio025@gmail.com>
2026-04-09 17:02:21 -0300

    ejercicio03

---
+++ Programa03/Programa03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Programa03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- BLOQUE 2: NÚMEROS CON DECIMALES ---");
+
+            // Estatura (double)
+            double estatura = 1.75;
+            Console.WriteLine("2. TIPO DECIMAL COMÚN (double):");
+            Console.WriteLine("   Valor guardado: " + estatura + " metros");
+
+            // Dinero (decimal) - Recuerda la 'm' al final
+            decimal precioProducto = 199.99m;
+            Console.WriteLine("\n3. TIPO DECIMAL FINANCIERO (decimal):");
+            Console.WriteLine("   ¡Obligatorio para dinero! Usa la 'm'.");
+            Console.WriteLine("   Valor guardado: $" + precioProducto);
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+    }
+}
