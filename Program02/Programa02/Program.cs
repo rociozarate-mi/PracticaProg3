e91ff20a8cbdef96223747aa1a0b5f483c44aaab
rociozarate-mi <zaraterocio025@gmail.com>
2026-04-09 16:53:17 -0300

    ejercicio 02

---
+++ Program02/Programa02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Programa02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            Console.WriteLine("--- BLOQUE 1: NÚMEROS ENTEROS ---");
+
+            // Declaración de un entero
+            int edad = 28;
+
+            Console.WriteLine("1. TIPO ENTERO (int):");
+            Console.WriteLine("   Para números sin decimales, como edad o cantidad.");
+            Console.WriteLine("   Valor guardado: " + edad);
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+    }
+}
