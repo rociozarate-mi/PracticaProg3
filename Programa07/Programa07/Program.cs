33d928c92997b0bdb9928ebe742b02e0fbf4fa8c
rociozarate-mi <zaraterocio025@gmail.com>
2026-04-10 12:34:53 -0300

    ejercicio 07

---
+++ Programa07/Programa07/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Programa07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // --- CONSTANTES DE CÁLCULO Y CONTROL ---
+            const decimal TASA_IVA = 0.21m; // 21% fijo
+            const bool MODO_PRUEBA = false; // Control de seguridad
+            const char SIMBOLO_PESO = '$';
+
+            Console.WriteLine("--- MÓDULO DE FACTURACIÓN ---");
+            Console.WriteLine("Modo prueba activo: " + MODO_PRUEBA);
+
+            decimal precioBase = 1500.50m;
+            decimal impuestoCalculado = precioBase * TASA_IVA;
+            decimal total = precioBase + impuestoCalculado;
+
+            Console.WriteLine("\nDetalle de la compra:");
+            Console.WriteLine("Precio: " + SIMBOLO_PESO + precioBase);
+            Console.WriteLine("IVA (" + (TASA_IVA * 100) + "%): " + SIMBOLO_PESO + impuestoCalculado);
+            Console.WriteLine("TOTAL A PAGAR: " + SIMBOLO_PESO + total);
+
+            Console.ReadLine();
+
+    }
+}
