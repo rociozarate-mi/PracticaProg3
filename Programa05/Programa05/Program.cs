c3c488883bc82f4b342b30c38cbe83c992165771
rociozarate-mi <zaraterocio025@gmail.com>
2026-04-10 12:23:28 -0300

    ejercicio 05

---
+++ Programa05/Programa05/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Programa05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- BLOQUE 4: VALORES LÓGICOS (SÍ / NO) ---");
+
+            // Booleano (bool) - Solo true o false
+            bool cursoCompletado = true;
+
+            Console.WriteLine("6. TIPO LÓGICO (bool):");
+            Console.WriteLine("   Solo puede ser 'true' o 'false'.");
+            Console.WriteLine("   ¿El curso está completado?: " + cursoCompletado);
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+    }
+}
